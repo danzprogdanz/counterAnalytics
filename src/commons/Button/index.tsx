ef55e41582fa8e651e30910dfcc71ab78056f58e
Daniel Viana <danzprogdanz@gmail.com>
2023-08-27 15:25:59 -0300

    [fix]: change btt tag

--- src/commons/Button/index.tsx
+++ src/commons/Button/index.tsx
@@ -1,27 +1,34 @@
 import styled from 'styled-components';
 import iconAdd from '../../assets/icons/material-symbols_add.svg';
 
-const Button = styled.div`
-    height: 30px;
-    width: 30px;
+const Button = styled.button`
+    height: 50px;
+    width: 50px;
 
     background-color: rgba(0, 0, 0, 0);
     border: 2px solid #000; 
     border-radius: 10px;
-    padding: 10px; /* Adjust the padding as needed */
     font-size: 16px; /* Adjust the font size as needed */
 
     border: 3px solid white;
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     &:hover {
-        background-color: #000; /* Change the background color on hover */
-        color: #fff; /* Change the text color on hover */
+        background-color: white; 
+        color: #fff; 
   }
 `;
 
 const Btn = () => {
 
-    return (<Button><img height='30px' src={iconAdd} alt='' /></Button>)
+    const handleTest = () => {
+        console.log('testando');
+    }
+
+    return (<Button onClick={handleTest}><img height='30px' src={iconAdd} alt='' /></Button>)
 }
 
 export default Btn;
